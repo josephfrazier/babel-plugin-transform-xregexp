6c588d6c33653fd1343dae489fdac7c903e286be
Joseph Frazier <1212jtraceur@gmail.com>
2017-02-19 00:52:20 -0500

    Use 2 spaces for indentation

--- test/index.js
+++ test/index.js
@@ -11,69 +11,69 @@ require('babel-register');
 var pluginPath = require.resolve('../src');
 
 function runTests() {
-	var testsPath = __dirname + '/fixtures/';
+  var testsPath = __dirname + '/fixtures/';
 
-	var exitCode = fs.readdirSync(testsPath).map(function(item) {
-		return {
-			path: path.join(testsPath, item),
-			name: item,
-		};
-	}).filter(function(item) {
-		return fs.statSync(item.path).isDirectory();
-	}).map(runTest).reduce((acc, cur) => acc + cur, 0);
+  var exitCode = fs.readdirSync(testsPath).map(function(item) {
+    return {
+      path: path.join(testsPath, item),
+      name: item,
+    };
+  }).filter(function(item) {
+    return fs.statSync(item.path).isDirectory();
+  }).map(runTest).reduce((acc, cur) => acc + cur, 0);
 
-	return exitCode;
+  return exitCode;
 }
 
 function runTest(dir) {
-	var exitCode = 0;
-	var output = babel.transformFileSync(dir.path + '/actual.js', {
-		babelrc: false,
-		plugins: [pluginPath]
-	});
+  var exitCode = 0;
+  var output = babel.transformFileSync(dir.path + '/actual.js', {
+    babelrc: false,
+    plugins: [pluginPath]
+  });
 
-	var expected = fs.readFileSync(dir.path + '/expected.js', 'utf-8');
+  var expected = fs.readFileSync(dir.path + '/expected.js', 'utf-8');
 
-	function normalizeLines(str) {
-		return str.trimRight().replace(/\r\n/g, '\n');
-	}
+  function normalizeLines(str) {
+    return str.trimRight().replace(/\r\n/g, '\n');
+  }
 
-	process.stdout.write(chalk.bgWhite.black(dir.name));
-	process.stdout.write('\n\n');
+  process.stdout.write(chalk.bgWhite.black(dir.name));
+  process.stdout.write('\n\n');
 
-	diff.diffLines(normalizeLines(output.code), normalizeLines(expected))
-	.forEach(function (part) {
-		var value = part.value;
-		if (part.added) {
-			value = chalk.green(part.value);
-			exitCode = 1;
-		} else if (part.removed) {
-			value = chalk.red(part.value);
-			exitCode = 1;
-		}
+  diff.diffLines(normalizeLines(output.code), normalizeLines(expected))
+  .forEach(function (part) {
+    var value = part.value;
+    if (part.added) {
+      value = chalk.green(part.value);
+      exitCode = 1;
+    } else if (part.removed) {
+      value = chalk.red(part.value);
+      exitCode = 1;
+    }
 
 
-		process.stdout.write(value);
-	});
+    process.stdout.write(value);
+  });
 
-	process.stdout.write('\n\n\n');
+  process.stdout.write('\n\n\n');
 
-	return exitCode;
+  return exitCode;
 }
 
 if (process.argv.indexOf('--watch') >= 0) {
-	require('watch').watchTree(__dirname + '/..', function () {
-		delete require.cache[pluginPath];
-		clear();
-		console.log('Press Ctrl+C to stop watching...');
-		console.log('================================');
-		try {
-			runTests();
-		} catch (e) {
-			console.error(chalk.magenta(e.stack));
-		}
-	});
+  require('watch').watchTree(__dirname + '/..', function () {
+    delete require.cache[pluginPath];
+    clear();
+    console.log('Press Ctrl+C to stop watching...');
+    console.log('================================');
+    try {
+      runTests();
+    } catch (e) {
+      console.error(chalk.magenta(e.stack));
+    }
+  });
 } else {
-	var exitCode = runTests();
-	process.exit(exitCode);
+  var exitCode = runTests();
+  process.exit(exitCode);
 }
