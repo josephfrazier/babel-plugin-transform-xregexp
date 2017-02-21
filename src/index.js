232da2fd4a794ebfc60aefa55ca95e034559c4e7
Joseph Frazier <1212jtraceur@gmail.com>
2017-02-21 16:23:09 -0500

    Use `standard` for linting

--- src/index.js
+++ src/index.js
@@ -1,54 +1,54 @@
-import XRegExp from "xregexp";
+import XRegExp from 'xregexp'
 
-function createRegExpLiteral(args, t) {
-  const evaluatedArgs = args.map((a) => a.evaluate());
+function createRegExpLiteral (args, t) {
+  const evaluatedArgs = args.map((a) => a.evaluate())
   if (!evaluatedArgs.every((a) => a.confident === true &&
-      typeof a.value === "string")) {
-    return;
+      typeof a.value === 'string')) {
+    return
   }
   const pattern = (evaluatedArgs.length >= 1 &&
-                    evaluatedArgs[0].value !== "") ?
-                  evaluatedArgs[0].value :
-                  "(?:)";
-  const flags = evaluatedArgs.length >= 2 ?
-                evaluatedArgs[1].value :
-                "";
+                    evaluatedArgs[0].value !== '')
+                  ? evaluatedArgs[0].value
+                  : '(?:)'
+  const flags = evaluatedArgs.length >= 2
+                ? evaluatedArgs[1].value
+                : ''
 
-  const xregexp = new XRegExp(pattern, flags);
+  const xregexp = new XRegExp(pattern, flags)
 
-  return t.regExpLiteral(xregexp.source, xregexp.flags);
+  return t.regExpLiteral(xregexp.source, xregexp.flags)
 }
 
-function maybeReplaceWithRegExpLiteral(path, t) {
-  if (!t.isIdentifier(path.node.callee, {name: "RegExp"})) {
-    return;
+function maybeReplaceWithRegExpLiteral (path, t) {
+  if (!t.isIdentifier(path.node.callee, {name: 'RegExp'})) {
+    return
   }
-  const regExpLiteral = createRegExpLiteral(path.get("arguments"), t);
+  const regExpLiteral = createRegExpLiteral(path.get('arguments'), t)
   if (regExpLiteral) {
-    path.replaceWith(regExpLiteral);
+    path.replaceWith(regExpLiteral)
   }
 }
 
 export default function ({ types: t }) {
   return {
     visitor: {
-      NewExpression(path) {
-        maybeReplaceWithRegExpLiteral(path, t);
+      NewExpression (path) {
+        maybeReplaceWithRegExpLiteral(path, t)
       },
-      CallExpression(path) {
+      CallExpression (path) {
         // equivalent to `new RegExp()` according to §21.2.3
-        maybeReplaceWithRegExpLiteral(path, t);
+        maybeReplaceWithRegExpLiteral(path, t)
       },
-      RegExpLiteral(path) {
-        const { node } = path;
+      RegExpLiteral (path) {
+        const { node } = path
 
-        const xregexp = XRegExp(node.pattern, node.flags);
-        const { source, flags } = xregexp;
+        const xregexp = XRegExp(node.pattern, node.flags)
+        const { source, flags } = xregexp
 
         if (source !== node.pattern || flags !== node.flags) {
-          path.replaceWith(t.RegExpLiteral(source, flags));
+          path.replaceWith(t.RegExpLiteral(source, flags))
         }
       }
     }
-  };
+  }
 }
--- test/index.js
+++ test/index.js
@@ -1,79 +1,77 @@
-var assert = require('assert');
-var babel = require('babel-core');
-var chalk = require('chalk');
-var clear = require('clear');
-var diff = require('diff');
-var fs = require('fs');
-var path = require('path');
+var babel = require('babel-core')
+var chalk = require('chalk')
+var clear = require('clear')
+var diff = require('diff')
+var fs = require('fs')
+var path = require('path')
 
-require('babel-register');
+require('babel-register')
 
-var pluginPath = require.resolve('../src');
+var pluginPath = require.resolve('../src')
 
-function runTests() {
-  var testsPath = __dirname + '/fixtures/';
+function runTests () {
+  var testsPath = path.resolve(__dirname, 'fixtures')
 
-  var exitCode = fs.readdirSync(testsPath).map(function(item) {
+  var exitCode = fs.readdirSync(testsPath).map(function (item) {
     return {
       path: path.join(testsPath, item),
-      name: item,
-    };
-  }).filter(function(item) {
-    return fs.statSync(item.path).isDirectory();
-  }).map(runTest).reduce((acc, cur) => acc + cur, 0);
+      name: item
+    }
+  }).filter(function (item) {
+    return fs.statSync(item.path).isDirectory()
+  }).map(runTest).reduce((acc, cur) => acc + cur, 0)
 
-  return exitCode;
+  return exitCode
 }
 
-function runTest(dir) {
-  var exitCode = 0;
+function runTest (dir) {
+  var exitCode = 0
   var output = babel.transformFileSync(dir.path + '/actual.js', {
     babelrc: false,
     plugins: [pluginPath]
-  });
+  })
 
-  var expected = fs.readFileSync(dir.path + '/expected.js', 'utf-8');
+  var expected = fs.readFileSync(dir.path + '/expected.js', 'utf-8')
 
-  function normalizeLines(str) {
-    return str.trimRight().replace(/\r\n/g, '\n').replace(/\(\?:\)/g, '');;
+  function normalizeLines (str) {
+    return str.trimRight().replace(/\r\n/g, '\n').replace(/\(\?:\)/g, '')
   }
 
-  process.stdout.write(chalk.bgWhite.black(dir.name));
-  process.stdout.write('\n\n');
+  process.stdout.write(chalk.bgWhite.black(dir.name))
+  process.stdout.write('\n\n')
 
   diff.diffLines(normalizeLines(output.code), normalizeLines(expected))
   .forEach(function (part) {
-    var value = part.value;
+    var value = part.value
     if (part.added) {
-      value = chalk.green(part.value);
-      exitCode = 1;
+      value = chalk.green(part.value)
+      exitCode = 1
     } else if (part.removed) {
-      value = chalk.red(part.value);
-      exitCode = 1;
+      value = chalk.red(part.value)
+      exitCode = 1
     }
 
+    process.stdout.write(value)
+  })
 
-    process.stdout.write(value);
-  });
+  process.stdout.write('\n\n\n')
 
-  process.stdout.write('\n\n\n');
-
-  return exitCode;
+  return exitCode
 }
 
 if (process.argv.indexOf('--watch') >= 0) {
-  require('watch').watchTree(__dirname + '/..', function () {
-    delete require.cache[pluginPath];
-    clear();
-    console.log('Press Ctrl+C to stop watching...');
-    console.log('================================');
+  require('watch').watchTree(path.resolve(__dirname, '/..'), function () {
+    delete require.cache[pluginPath]
+    clear()
+    console.log('Press Ctrl+C to stop watching...')
+    console.log('================================')
     try {
-      runTests();
+      runTests()
     } catch (e) {
-      console.error(chalk.magenta(e.stack));
+      console.error(chalk.magenta(e.stack))
     }
-  });
+  })
 } else {
-  var exitCode = runTests();
-  process.exit(exitCode);
+  var exitCode = runTests()
+  process.exit(exitCode)
 }
