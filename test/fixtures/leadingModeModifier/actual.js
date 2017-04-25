adcbe084762e2478436645752aee47ebc5eaf33b
Joseph Frazier <1212jtraceur@gmail.com>
2017-04-25 04:12:57 -0400

    Add semicolons to test inputs

--- test/fixtures/leadingModeModifier/actual.js
+++ test/fixtures/leadingModeModifier/actual.js
@@ -1,2 +1,2 @@
-/(?im)^[a-z]+$/
-new RegExp('(?im)^[a-z]+$')
+/(?im)^[a-z]+$/;
+new RegExp('(?im)^[a-z]+$');
--- test/fixtures/namedCapture/actual.js
+++ test/fixtures/namedCapture/actual.js
@@ -1,2 +1,2 @@
-/\b(?<word>[a-z]+)\s+\k<word>\b/gi
-new RegExp('\\b(?<word>[a-z]+)\\s+\\k<word>\\b', 'gi')
+/\b(?<word>[a-z]+)\s+\k<word>\b/gi;
+new RegExp('\\b(?<word>[a-z]+)\\s+\\k<word>\\b', 'gi');
